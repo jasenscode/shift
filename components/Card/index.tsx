fb6c87b226a7e2f4885c496ea91793de165fbd66
Jasen <60624556+jasenscode@users.noreply.github.com>
2024-02-13 14:41:21 +0000

    feat: :sparkles: Created card component

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -3,17 +3,19 @@ import { ReactNode } from 'react';
 import { ViewStyle } from 'react-native';
 import { SIZES, COLORS, SHADOWS } from '../../constants/theme';
 
-type Props = { children: ReactNode; styles?: ViewStyle };
+type Props = { children: ReactNode; style?: ViewStyle };
 
-export const Card = ({ children, styles }: Props) => {
-  return <View style={[defaultStyles.container, styles]}>{children}</View>;
+export const Card = ({ children, style }: Props) => {
+  return <View style={[defaultStyles.container, style]}>{children}</View>;
 };
 
 const defaultStyles = StyleSheet.create({
   container: {
-    backgroundColor: COLORS.primary,
+    backgroundColor: COLORS.dark,
+    borderWidth: 1,
+    borderColor: COLORS.grey4,
     borderRadius: 10,
     padding: SIZES.medium,
-    ...SHADOWS.small,
+    // ...SHADOWS.medium,
   },
 });
